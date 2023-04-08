43d336b5d8038ba191a815a2b9b6ba52ccfcab16
Muhammed Fawaz MV <mvfawazmfz@gmail.com>
2023-04-08 13:44:19 +0530

    deploy errors

--- src/App/App.js
+++ src/App/App.js
@@ -24,7 +24,7 @@ const AppRouter = createBrowserRouter([
 
     {
         path: '/turf-admin',
-        element: (<Suspense><ApplayoutTurf /> </Suspense>),
+        element: (<ApplayoutTurf />),
         children: TurfRoutes
 
     },
