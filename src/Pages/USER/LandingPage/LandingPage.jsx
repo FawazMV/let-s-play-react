6e7de0db171d5e5b6a5d9588dc33d4ce6d898ded
Muhammed Fawaz MV <mvfawazmfz@gmail.com>
2023-04-10 17:40:17 +0530

    responsive updations

--- src/Pages/USER/LandingPage/LandingPage.jsx
+++ src/Pages/USER/LandingPage/LandingPage.jsx
@@ -6,12 +6,12 @@ const LandingPage = () => {
         <div className="flex justify-evenly items-end bg-slate-800 p-10 md:p-24 w-full h-screen lg:flex-wrap flex-wrap-reverse">
             <div className='md:w-[65%] lg:w-[37%] xl:w-[32%] mt-5 lg:mt-8'>
                 <h1 className='text-5xl text-white  font-extrabold'>Your Playground is just a tap away...</h1>
-                <div className='flex w-full  lg:justify-between justify-evenly flex-wrap pt-5 lg:pt-8'>
-                    <Link to={'/turfs'}> <Button color={'bg-green-500'}>To Book Avenue</Button> </Link>
-                    <Link to={'/register-turf'}><Button color={'bg-yellow-400'}>To Get Listed</Button></Link>
+                <div className='flex w-full lg:justify-between justify-evenly flex-wrap pt-5 lg:pt-8'>
+                    <Link className='w-full xs:w-auto' to={'/turfs'}> <Button color={'bg-green-500'}>To Book Avenue</Button> </Link>
+                    <Link className='w-full xs:w-auto' to={'/register-turf'}><Button color={'bg-yellow-400 px-[33px]'}>To Get Listed</Button></Link>
                 </div>
             </div>
-            <div className='mt-[1000px] md:mt-0'>
+            <div className='mt-[1200px] md:mt-0'>
                 <img className='w-72' src={img} />
             </div>
         </div >
--- src/Pages/USER/Layout/Button.jsx
+++ src/Pages/USER/Layout/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = (props) => {
     return (
-        <button className={`${props.color} text-white mb-2 font-[Poppins] py-2 px-6 rounded  hover:bg-indigo-400 
+        <button className={`${props.color} text-white mb-2 font-[Poppins] py-2 w-full xs:w-auto px-6 rounded  hover:bg-indigo-400 
     duration-500`}>
             {props.children}
         </button>
